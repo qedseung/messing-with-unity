bb272e96e1e2a8ec7ecea197c19a55a0bf4de884
steven seung <qedseung@gmail.com>
2017-10-05 17:43:02 -0700

    first commit

---
+++ Assets/RocketLauncher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLauncher : MonoBehaviour {
+
+	public bool launched = false;
+	public Transform rocket;
+	// Use this for initialization
+	void OnTriggerEnter (Collider col) {
+		print ("Launch Rocket");
+		launched = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (launched) {
+			rocket.position = new Vector3 (rocket.position.x, rocket.position.y + 1f, rocket.position.z);
+		}
+	}
+}
